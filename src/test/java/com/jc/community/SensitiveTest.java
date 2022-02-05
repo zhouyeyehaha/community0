c83ab541e6a813df70ee9c6f640eb5e9ccd31f55
zjc <zjc@qq.com>
2022-02-05 23:48:09 +0800

    20220205

---
+++ src/test/java/com/jc/community/SensitiveTest.java
@@ -0,0 +1,20 @@
+package com.jc.community;
+
+import com.jc.community.util.SensitiveFiler;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SensitiveTest {
+
+    @Autowired
+    private SensitiveFiler sensitiveFiler;
+
+    @Test
+    public void test(){
+        String string = "这里可以赌☆博,哈哈哈";
+        String filter = sensitiveFiler.filter(string);
+        System.out.println(filter);
+    }
+}
