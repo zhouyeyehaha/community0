3c1ba2e34b0d9463220fbf8c82dac0a3b645f45c
zjc <zjc@qq.com>
2022-02-09 21:28:07 +0800

    20220209

---
+++ src/test/java/com/jc/community/RedisTest.java
@@ -0,0 +1,88 @@
+package com.jc.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testString() {
+        String redisKey = "test:count";
+
+        redisTemplate.opsForValue().set(redisKey, 1);
+
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void testHash() {
+        String redisKey = "test:user";
+
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "username", "zhangsan");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "username"));
+    }
+
+    @Test
+    public void test03() {
+        String redisKey = "test:id";
+
+        redisTemplate.opsForList().leftPush(redisKey, 101);
+        redisTemplate.opsForList().leftPush(redisKey, 102);
+        redisTemplate.opsForList().leftPush(redisKey, 103);
+
+        System.out.println(redisTemplate.opsForList().size(redisKey));
+        System.out.println(redisTemplate.opsForList().index(redisKey, 0));
+        System.out.println(redisTemplate.opsForList().range(redisKey, 0, 2));
+
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+    }
+
+    @Test
+    public void test02() {
+        String redisKey = "test:teachers";
+
+        redisTemplate.opsForSet().add(redisKey, "一号", "二号", "三号");
+
+        System.out.println(redisTemplate.opsForSet().size(redisKey));
+        System.out.println(redisTemplate.opsForSet().pop(redisKey));
+        System.out.println(redisTemplate.opsForSet().members(redisKey));
+    }
+
+    @Test
+    public void test04() {
+        String redisKey = "test:students";
+
+        redisTemplate.opsForZSet().add(redisKey, "一号", 1);
+        redisTemplate.opsForZSet().add(redisKey, "二号", 2);
+        redisTemplate.opsForZSet().add(redisKey, "三号", 3);
+
+        System.out.println(redisTemplate.opsForZSet().zCard(redisKey));
+        System.out.println(redisTemplate.opsForZSet().score(redisKey, "一号"));
+        System.out.println(redisTemplate.opsForZSet().reverseRank(redisKey,"二号"));
+        System.out.println(redisTemplate.opsForZSet().range(redisKey,0,1));
+    }
+
+    @Test
+    public void test05() {
+        redisTemplate.delete("test:user");
+
+        System.out.println(redisTemplate.hasKey("test:user"));
+
+        redisTemplate.expire("test:students", 10, TimeUnit.SECONDS);
+    }
+}
